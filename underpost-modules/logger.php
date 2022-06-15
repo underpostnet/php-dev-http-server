5ad91f51ae2bedb9602eb1a8b6535387b1b84daf
fcoverdugo <fcoverdugoa@underpost.net>
2022-06-15 09:59:37 -0400

    update

--- underpost-modules/logger.php
+++ underpost-modules/logger.php
@@ -3,6 +3,9 @@
 class logger {
 
   function log($str, $dump = false){
+    if($this->mute){
+      return;
+    }
     ob_start();
     $dump ? var_dump($str) : print_r($str);
     error_log(ob_get_clean(), 4);
@@ -10,14 +13,22 @@ class logger {
 
   public function __construct(){
     $this->dataColor = json_decode(file_get_contents('./data/colors.json'));
+    $this->mute = false;
   }
   // http://www.unixwerk.eu/unix/ansicodes.html
 
   function color($color = "white-black", $str){
+    if($this->mute){
+      return;
+    }
     $dc_ = explode("-", $color);
     $codeTX = $dc_[0];
     $codeBG = (array_key_exists(1, $dc_)) ? $dc_[1] : "black";
     $this->log("\033[1;".$this->dataColor->text->$codeTX.";".$this->dataColor->background->$codeBG."m".$str."\033[0m\n");
+  }
+
+  function mute(){
+    $this->mute = true;
   }
 
 }
