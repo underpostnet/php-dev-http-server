b7669057f8288f6c3aafd16a04e76aca12800019
fcoverdugo <fcoverdugoa@underpost.net>
2022-06-21 19:10:38 -0400

    update

--- underpost-modules/logger.php
+++ underpost-modules/logger.php
@@ -12,7 +12,30 @@ class logger {
   }
 
   public function __construct(){
-    $this->dataColor = json_decode(file_get_contents('./data/colors.json'));
+    $this->dataColor = json_decode('{
+      "text":{
+         "black":"30",
+         "red":"31",
+         "green":"32",
+         "yellow":"33",
+         "blue":"34",
+         "magenta":"35",
+         "cyan":"36",
+         "white":"37",
+         "default":"39"
+      },
+      "background":{
+         "black":"40",
+         "red":"41",
+         "green":"42",
+         "yellow":"43",
+         "blue":"44",
+         "magenta":"45",
+         "cyan":"46",
+         "white":"47",
+         "default":"49"
+      }
+   }');
     $this->mute = false;
   }
   // http://www.unixwerk.eu/unix/ansicodes.html
