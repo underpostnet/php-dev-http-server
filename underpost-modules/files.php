dbe044ebd374f9e8e759a372a2d276cf438dcde4
fcoverdugo <fcoverdugoa@underpost.net>
2022-06-21 18:32:59 -0400

    update

--- underpost-modules/files.php
+++ underpost-modules/files.php
@@ -1,51 +1,6 @@
 <?php
 
-
 // exist - write(update/create) - delete - rename
-
-// recursive iterator
-
-
-
-// delete recursive
-/*
-function rrmdir($dir) {
-   if (is_dir($dir)) {
-     $objects = scandir($dir);
-     foreach ($objects as $object) {
-       if ($object != "." && $object != "..") {
-         if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
-           rrmdir($dir. DIRECTORY_SEPARATOR .$object);
-         else
-           unlink($dir. DIRECTORY_SEPARATOR .$object);
-       }
-     }
-     rmdir($dir);
-   }
- }
-
-
-
- copy recursiuve
-
- $source = "dir/dir/dir";
-$dest= "dest/dir";
-
-mkdir($dest, 0755);
-foreach (
- $iterator = new \RecursiveIteratorIterator(
-  new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS),
-  \RecursiveIteratorIterator::SELF_FIRST) as $item
-) {
-  if ($item->isDir()) {
-    mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname());
-  } else {
-    copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname());
-  }
-}
-
-
- */
 
 // file_exists($path_test)
 
@@ -59,27 +14,6 @@ fclose($file);
 
 // json_decode option enable edit -> , true
 json_decode(file_get_contents( "./domains.json" ));
-
-
-
-function recursiveIteratorDirectory($ruta){
-  if (is_dir($ruta)) {
-    if ($dh = opendir($ruta)) {
-      while (($file = readdir($dh)) !== false) {
-
-        $path = $ruta . $file;
-
-
-        if (is_dir($ruta . $file) && $file!='.' && $file!='..'){
-
-          recursiveIteratorDirectory($ruta . $file . '/');
-
-        }
-      }
-      closedir($dh);
-    }
-  }
-}
 
 */
 
